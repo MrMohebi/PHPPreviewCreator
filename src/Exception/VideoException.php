ef4acebef22139d1a9387828a544da978b007d25
MMMohebi <MMMohebi@outlook.com>
2022-03-26 07:57:35 +0430

    push

---
+++ src/Exception/VideoException.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace MrMohebi\PHPPreviewCreator\Exception;
+
+
+/**
+* VideoException.
+ *
+ * @author Antal Áron <antalaron@antalaron.hu>
+ */
+class VideoException extends \RuntimeException
+{
+}
